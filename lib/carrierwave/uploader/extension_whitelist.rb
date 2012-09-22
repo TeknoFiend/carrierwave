82f211f0d49f4612ba5704cb2f24801603276eed
Weston Sewell <westons@spiceworks.com>
2012-09-22 00:58:44 -0500

    - Not paying attention, copied wrong bit of code. Should put in tests and/or fix existing ones.

--- lib/carrierwave/uploader/extension_whitelist.rb
+++ lib/carrierwave/uploader/extension_whitelist.rb
@@ -36,7 +36,7 @@ module CarrierWave
       def extension_white_list; end
 
       def extension_white_list_error
-        I18n.translate(:"errors.messages.extension_white_list_error")
+        :"errors.messages.extension_white_list_error"
       end
 
       def allowed_types_message
@@ -48,7 +48,7 @@ module CarrierWave
       def check_whitelist!(new_file)
         extension = new_file.extension.to_s
         if extension_white_list and not extension_white_list.detect { |item| extension =~ /\A#{item}\z/i }
-          raise CarrierWave::IntegrityError, extension_white_list_error, :extension => new_file.extension.inspect, :allowed_types => allowed_types_message)
+          raise CarrierWave::IntegrityError, I18n.translate(extension_white_list_error, :extension => new_file.extension.inspect, :allowed_types => allowed_types_message)
         end
       end
 
